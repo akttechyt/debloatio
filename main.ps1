35f8ffa3a7f477e6f3ad6aafa1082664edc500ba
akt <akttechinq@outlook.com>
2025-03-02 19:31:39 -0700

    Many updates

--- main.ps1
+++ main.ps1
@@ -1,3 +1,12 @@
+# Debloatio v0.1.0
+
+# This is the important code, containing the function powering Yes/No questions.
+# Write-Host colour guide:
+# Yellow: Warnings, important stuff
+# Red: Errors, extremely important stuff
+# Blue: Info
+# Green: Success
+
 function YesNoQuestions {
     param (
         [Parameter(Mandatory = $true)]
@@ -15,17 +24,30 @@ function YesNoQuestions {
     } while ($true)
 }
 
-Write-Host "DEBLOATIO | v0.0.0"
+Write-Host "Debloatio (alpha) | v0.2.0 (Beta stage update)" -ForegroundColor Blue
 Write-Host "WARNING | This is alpha software, which means it is EXTREMELY UNSTABLE. Recommended for testing only on a VIRTUAL MACHINE or A PC WHICH YOU DON'T HAVE IMPORTANT DATA ON." -ForegroundColor Yellow
 Write-Host "WARNING | Creator or contributors are NOT RESPONSIBLE for ANY DAMAGE this causes to your PC." -ForegroundColor Yellow
-$sysRestore = YesNoQuestions -Question "Make a System Restore point?"
-if ($sysRestore) {
-    Checkpoint-Computer -Description "DebloatioRP" -RestorePointType "MODIFY_SETTINGS"
-}
-$shouldProcessStart = YesNoQuestions -Question "Read all the warnings related to the program and agree?"
+
+$shouldProcessStart = YesNoQuestions -Question "Have you read and agree to the warnings and wish to start this program?"
 
 if ($shouldProcessStart) {
-    Get-AppxPackage | Remove-AppxPackage
+    # Main code
+
+    # Ask to create a restore point
+    $sysRestore = YesNoQuestions -Question "Make a System Restore point?"
+    if ($sysRestore) {
+        Checkpoint-Computer -Description "PreDebloatio" -RestorePointType "MODIFY_SETTINGS"
+        Write-Host "Restore point created. Find the 'PreDebloatio' point if any errors occur." -ForegroundColor Blue
+    }
+
+    Write-Host "The process is starting. Please wait a few minutes. Some functionality may break during the process; it's best to just not use this device until the process has completed." -ForegroundColor Blue
+
+    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarDa" -Value 0
+    Write-Host "Turned off Widgets" -ForegroundColor Green
+
+    Get-AppxPackage -AllUsers | Remove-AppxPackage
+
+
 }
 else {
     exit
